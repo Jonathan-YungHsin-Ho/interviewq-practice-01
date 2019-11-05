aa8c1d05824648851896c0ed34408a58dbec1287
Jonathan Ho <johnnyho82@gmail.com>
2019-11-05 11:16:27 -0500

    Prepare for Heroku deployment

---
+++ index.js
@@ -0,0 +1,29 @@
+const { ApolloServer, gql } = require('apollo-server');
+const { buildFederatedSchema } = require('@apollo/federation');
+
+const typeDefs = gql`
+  type Query {
+    interviewQMessage: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    interviewQMessage: () => `Hello World from Interview Q Backend API!`,
+  },
+};
+
+const server = new ApolloServer({
+  schema: buildFederatedSchema([
+    {
+      typeDefs,
+      resolvers,
+    },
+  ]),
+});
+
+const port = process.env.PORT || 4002;
+
+server.listen(port, () =>
+  console.log(`\n** Interview Q server listening on port ${port} **\n`),
+);
